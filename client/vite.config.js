4774845a54641e921e11289b94aa3b61c03491bb
Anushka Garg <cs21b1032@iiitr.ac.in>
2024-09-22 15:37:46 +0530

    configuration changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,41 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   resolve: {
+//     alias: {
+//       path: 'path-browserify', 
+//       url: 'url-polyfill', 
+//     },
+//   },
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:3000',
+//         secure: false,
+//       },
+//     },
+//   },
+//   optimizeDeps: {
+//     exclude: ['source-map-js', 'fs'],
+//   },
+//   build: {
+//     rollupOptions: {
+//       input: {
+//         main: '/src/main.jsx',
+//       },
+//     },
+//   },
+  
+//   plugins: [react()],
+// })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      path: 'path-browserify', 
-      url: 'url-polyfill', 
-    },
-  },
   server: {
     proxy: {
       '/api': {
@@ -17,16 +44,6 @@ export default defineConfig({
       },
     },
   },
-  optimizeDeps: {
-    exclude: ['source-map-js', 'fs'],
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        main: '/src/main.jsx',
-      },
-    },
-  },
-  
+
   plugins: [react()],
-})
+});
